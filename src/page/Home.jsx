a0b24038acfff845b9b4b4e9bd4201832ba4f27f
therealJimoh <jimoh.abdullah69@gmail.com>
2022-06-05 03:10:39 +0100

    fix: fixed failed deployment

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Pagination from "../components/Pagination";
 import RandomUser from "../components/RandomUser";
